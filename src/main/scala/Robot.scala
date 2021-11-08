28ab54c85e5da8752d8eaf70aec1cd185ef3ca22
Stanislav Kalashnikov <stas@halega.com>
2021-11-08 08:51:38 +0300

    extend Robot class by ItalianRobot and EnglishRobot

--- src/main/scala/Robot.scala
+++ src/main/scala/Robot.scala
@@ -1,3 +1,11 @@
 class Robot(val name: String = "Unknown") {
   def welcome(n: String) = s"Welcome $n! My name is $name"
 }
+
+class ItalianRobot(name: String) extends Robot(name) {
+  override def welcome(n: String): String = s"Benvenuto $n! Il mio nome e' $name"
+}
+
+class EnglishRobot(name: String, country: String) extends Robot(name) {
+  override def welcome(n: String): String = s"Welcome $n, I am $name from the country of $country!"
+}

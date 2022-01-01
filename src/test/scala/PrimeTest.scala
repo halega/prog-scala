6b23ec307e818cefe8c43a74226aeac81d95d9ad
Stanislav Kalashnikov <stas@halega.com>
2022-01-01 18:04:08 +0300

    show problem numbers in isPrimeSimple tests

--- src/test/scala/PrimeTest.scala
+++ src/test/scala/PrimeTest.scala
@@ -1,20 +1,21 @@
 import Prime.isPrimeSimple
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
-class PrimeTest extends AnyFlatSpec {
+class PrimeTest extends AnyFlatSpec with Matchers {
 
   "isPrimeSimple" should "return true if n is prime" in {
     val primes = Seq(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41)
-    for (n <- primes) {
-      assert(isPrimeSimple(n))
-    }
+    val expected = primes.map((_, true))
+    val actual = primes.map(n => (n, isPrimeSimple(n)))
+    actual shouldBe expected
   }
 
   "isPrimeSimple" should "return false if n is not prime" in {
-    val primes = Seq(4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28)
-    for (n <- primes) {
-      assert(!isPrimeSimple(n))
-    }
+    val primes = Seq(3, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28)
+    val expected = primes.map((_, true))
+    val actual = primes.map(n => (n, !isPrimeSimple(n)))
+    assert(expected == actual)
   }
 
 }

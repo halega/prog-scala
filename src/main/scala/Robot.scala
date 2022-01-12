981a587c3f5664d2348cc270def9b4ac8bbae813
Stanislav Kalashnikov <stas@halega.com>
2022-01-12 08:41:25 +0300

    override welcome method

--- src/main/scala/Robot.scala
+++ src/main/scala/Robot.scala
@@ -1,11 +1,13 @@
 class Robot(n: String) {
   val name: String = "Robot " + n
+
+  def welcome(name: String): String = s"Hi $name! I'm ${this.name}"
 }
 
 class ItalianRobot(n: String) extends Robot(n) {
-  def welcome(name: String): String = s"Benvenuto $name! Il mio nome e' ${this.name}"
+  override def welcome(name: String): String = s"Benvenuto $name! Il mio nome e' ${this.name}"
 }
 
 class EnglishRobot(name: String, country: String) extends Robot(name) {
-  def welcome(n: String): String = s"Welcome $n, I am $name from the country of $country!"
+  override def welcome(n: String): String = s"Welcome $n, I am $name from the country of $country!"
 }
